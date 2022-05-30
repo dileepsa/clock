e98b484d83d31a5b12cc466dbf7c7bd849c6c7c2
Dileep A <99489054+dileepsa@users.noreply.github.com>
2022-05-30 11:41:13 +0530

    Added date day in Clock.

--- clock.js
+++ clock.js
@@ -9,6 +9,7 @@ const link = (rel, href) => `<link rel=${rel} href=${href}>`;
 class Clock {
   constructor() {
     this.date = new Date();
+    this.dateStr = this.date.toDateString();
     this.hours = this.date.getHours();
     this.minutes = this.date.getMinutes();
     this.seconds = this.date.getSeconds();
@@ -17,15 +18,18 @@ class Clock {
   toHtml() {
     const refresh = meta('refresh', 1);
     const linkTag = link('stylesheet', './styles.css');
+    const dateStr = div('date', this.dateStr);
     const hours = div('box', this.hours);
     const minutes = div('box', this.minutes);
     const seconds = div('box', this.seconds);
     const time = div('time', hours + minutes + seconds);
-    return refresh + linkTag + time;
+    const html = refresh + linkTag + dateStr + time
+    return html;
   }
 }
 
 setInterval(() => {
   const currentTime = new Clock();
   fs.writeFileSync('index.html', currentTime.toHtml(), 'utf-8');
+
 }, 1000);
