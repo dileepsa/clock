fd0d2b4dd52a8f8825827c6d9c5d03ddb8602320
Dileep A <99489054+dileepsa@users.noreply.github.com>
2022-05-30 11:12:57 +0530

    Added html to clock.

--- clock.js
+++ clock.js
@@ -1,4 +1,10 @@
+const fs = require('fs');
+
 const div = (classes, content) => `<div class=${classes}>${content}</div>`;
+
+const meta = (httpEquiv, content) => `<meta http-equiv=${httpEquiv} content=${content}>`;
+
+const link = (rel, href) => `<link rel=${rel} href=${href}>`;
 
 class Clock {
   constructor() {
@@ -9,14 +15,17 @@ class Clock {
   }
 
   toHtml() {
-    const hours = div('hours', this.hours);
-    const minutes = div('minutes', this.minutes);
-    const seconds = div('seconds', this.seconds);
-    return hours + minutes + seconds;
+    const refresh = meta('refresh', 1);
+    const linkTag = link('stylesheet', './styles.css');
+    const hours = div('box', this.hours);
+    const minutes = div('box', this.minutes);
+    const seconds = div('box', this.seconds);
+    const time = div('time', hours + minutes + seconds);
+    return refresh + linkTag + time;
   }
 }
 
 setInterval(() => {
   const currentTime = new Clock();
-  console.log(currentTime.toHtml());
+  fs.writeFileSync('index.html', currentTime.toHtml(), 'utf-8');
 }, 1000);
